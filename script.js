02e0c2b443e740e366b40e2efe537b42df4128ed
matt <mdeloux@gmail.com>
2021-12-20 11:56:47 -0700

    toggle event in script.js, style change to buttons in style.css, button rework in index.html

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
-document.querySelector("body").appendChild(h2);
+function togglediv() {
+    var myPersonal = document.getElementById('item');
+    var displaySetting = myPersonal.style.display;
+    var firstButton = document.getElementById('firstButton');
+    if (displaySetting == 'block') {
+        myPersonal.style.display = 'none';
+        firstButton.innerHTML = 'Personal Life';
+    } else {
+        myPersonal.style.display = 'block';
+        firstButton.innerHTML = 'Personal Life';
+    }
+}
+function toggledivtwo() {
+    var myPersonal = document.getElementById('work');
+    var displaySetting = myPersonal.style.display;
+    var firstButton = document.getElementById('secondButton');
+    if (displaySetting == 'block') {
+        myPersonal.style.display = 'none';
+        firstButton.innerHTML = 'Professional Life';
+    } else {
+        myPersonal.style.display = 'block';
+        firstButton.innerHTML = 'Professional Life';
+    }
+}
+function toggledivthree() {
+    var myPersonal = document.getElementById('fun');
+    var displaySetting = myPersonal.style.display;
+    var firstButton = document.getElementById('thirdButton');
+    if (displaySetting == 'block') {
+        myPersonal.style.display = 'none';
+        firstButton.innerHTML = 'Free Time';
+    } else {
+        myPersonal.style.display = 'block';
+        firstButton.innerHTML = 'Free Time';
+    }
+}
